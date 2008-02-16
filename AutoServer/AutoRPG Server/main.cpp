eed36ddc9950904abff4d41182652f3e88dbda7f
achew22 <achew22@b2d95fcb-6840-0410-91c4-7d0ded462287>
2008-02-16 07:18:03 +0000

    Added buffering and the "Exit"/"Quit" functions

--- AutoServer/AutoRPG Server/main.cpp
+++ AutoServer/AutoRPG Server/main.cpp
@@ -1,97 +1,20 @@
-#ifdef __cplusplus
-    #include <cstdlib>
-#else
-    #include <stdlib.h>
-#endif
-#ifdef __APPLE__
-#include <SDL/SDL.h>
-#else
-#include <SDL.h>
 #include <SDL_net.h>
 #include <string>
-#endif
+#include <iostream>
+
+const int AUTORPG_SERVER_PORT = 2000;
 
 int main ( int argc, char** argv )
 {
-/*
-    // initialize SDL video
-    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "Unable to init SDL: %s\n", SDL_GetError() );
-        return 1;
-    }
-    // make sure SDL cleans up before exit
-    atexit(SDL_Quit);
 
-    // create a new window
-    SDL_Surface* screen = SDL_SetVideoMode(640, 480, 16,
-                                           SDL_HWSURFACE|SDL_DOUBLEBUF);
-    if ( !screen )
-    {
-        printf("Unable to set 640x480 video: %s\n", SDL_GetError());
-        return 1;
-    }
+    //This code is gold, NEVER lose it
+    //This code remaps stderr and stdout back to where they were (Against the wishes of SDL_net for some reason);
+    freopen( "CON", "w", stdout );
+    freopen( "CON", "w", stderr );
 
-    // load an image
-    SDL_Surface* bmp = SDL_LoadBMP("cb.bmp");
-    if (!bmp)
-    {
-        printf("Unable to load bitmap: %s\n", SDL_GetError());
-        return 1;
-    }
+    std::cout << "Entering AutoRPG Server Main loop\n";
 
-    // centre the bitmap on screen
-    SDL_Rect dstrect;
-    dstrect.x = (screen->w - bmp->w) / 2;
-    dstrect.y = (screen->h - bmp->h) / 2;
-
-    // program main loop
-    bool done = false;
-    while (!done)
-    {
-        // message processing loop
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            // check for messages
-            switch (event.type)
-            {
-                // exit if the window is closed
-            case SDL_QUIT:
-                done = true;
-                break;
-
-                // check for keypresses
-            case SDL_KEYDOWN:
-                {
-                    // exit if ESCAPE is pressed
-                    if (event.key.keysym.sym == SDLK_ESCAPE)
-                        done = true;
-                    break;
-                }
-            } // end switch
-        } // end of message processing
-
-        // DRAWING STARTS HERE
-
-        // clear screen
-        SDL_FillRect(screen, 0, SDL_MapRGB(screen->format, 0, 0, 0));
-
-        // draw bitmap
-        SDL_BlitSurface(bmp, 0, screen, &dstrect);
-
-        // DRAWING ENDS HERE
-
-        // finally, update the screen :)
-        SDL_Flip(screen);
-    } // end main loop
-
-    // free loaded bitmap
-    SDL_FreeSurface(bmp);
-*/
-
-
-	TCPsocket sd, csd; /* Socket descriptor, Client socket descriptor */
+    TCPsocket sd, csd; // Socket descriptor, Client socket descriptor
 	IPaddress ip, *remoteIP;
 	int quit, quit2;
 	char buffer[512];
@@ -103,39 +26,39 @@ int main ( int argc, char** argv )
 		exit(EXIT_FAILURE);
 	}
 
-	/* Resolving the host using NULL make network interface to listen */
-	if (SDLNet_ResolveHost(&ip, NULL, 2000) < 0)
+	// Resolving the host using NULL make network interface to listen
+	if (SDLNet_ResolveHost(&ip, NULL, AUTORPG_SERVER_PORT) < 0)
 	{
 		fprintf(stderr, "SDLNet_ResolveHost: %s\n", SDLNet_GetError());
 		exit(EXIT_FAILURE);
 	}
 
-	/* Open a connection with the IP provided (listen on the host's port) */
+	// Open a connection with the IP provided (listen on the host's port)
 	if (!(sd = SDLNet_TCP_Open(&ip)))
 	{
 		fprintf(stderr, "SDLNet_TCP_Open: %s\n", SDLNet_GetError());
 		exit(EXIT_FAILURE);
 	}
 
-	/* Wait for a connection, send data and term */
+	// Wait for a connection, send data and term
 	quit = 0;
 	while (!quit)
 	{
-		/* This check the sd if there is a pending connection.
-		* If there is one, accept that, and open a new socket for communicating */
+		// This check the sd if there is a pending connection.
+		// If there is one, accept that, and open a new socket for communicating
         //fprintf(stdout, "Checking for connections\n");
 		if ((csd = SDLNet_TCP_Accept(sd)))
 		{
-            fprintf(stdout, "Got a connection\n");
-			/* Now we can communicate with the client using csd socket
-			* sd will remain opened waiting other connections */
+            std::cout << "Got a connection\n";
+			// Now we can communicate with the client using csd socket
+			// sd will remain opened waiting other connections
 
-			/* Get the remote address */
+			// Get the remote address
 			if ((remoteIP = SDLNet_TCP_GetPeerAddress(csd))) {
-				/* Print the address, converting in the host format */
-				printf("Host connected: %x %d\n", SDLNet_Read32(&remoteIP->host), SDLNet_Read16(&remoteIP->port));
+				// Print the address, converting in the host format
+				std::cout << "Host connected from " << SDLNet_Read32(&remoteIP->host) << " on port" << SDLNet_Read16(&remoteIP->port) << "\r\n";
 
-				std::string greeting = "Welcome to AutoServer for AutoRPG (http://autorpg.googlecode.com/)\n";
+				std::string greeting = "Welcome to AutoServer for AutoRPG (http://autorpg.googlecode.com/)\r\n";
 				SDLNet_TCP_Send(csd, greeting.c_str(), greeting.length() + 1);
 			}
 			else {
@@ -147,32 +70,31 @@ int main ( int argc, char** argv )
 			while (!quit2)
 			{
 
-				printf("Scanning buffer");
+				std::cout << "Buffer: " << messageBuffer << "\r\n";
 				if (SDLNet_TCP_Recv(csd, buffer, 512) > 0)
 				{
-				    if (strcmp(buffer, "\n") == 0) {
-                        printf("Client say: %s\n", buffer);
-
-                        if(strcmp(messageBuffer.c_str(), "exit") == 0)	/* Terminate this connection */
+				    if ((int)buffer[0] == 13) {
+                        if(messageBuffer == "exit")	// Terminate this connection
                         {
-                            //quit2 = 1;
-                            printf("Terminate connection\n");
+                            quit2 = 1;
+                            std::cout << "Terminate connection\n";
                         }
-                        if(strcmp(messageBuffer.c_str(), "quit") == 0)	/* Quit the program */
+                        if(messageBuffer == "quit") // Quit the program
                         {
-                            //quit2 = 1;
-                            //quit = 1;
-                            printf("Quit program\n");
+                            quit2 = 1;
+                            quit = 1;
+                            std::cout << "Quit program\n";
                         }
                         messageBuffer = "";
+                        std::cout << "Got newline\n";
                     }
                     else {
-                        messageBuffer += buffer;
+                        messageBuffer = messageBuffer + buffer[0];
                     }
 				}
 			}
 
-			/* Close the client socket */
+			// Close the client socket
 			SDLNet_TCP_Close(csd);
 		}
 	}
@@ -189,4 +111,5 @@ int main ( int argc, char** argv )
     // all is well ;)
     printf("Exited cleanly\n");
     return 0;
+
 }

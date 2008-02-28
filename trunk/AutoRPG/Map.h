9a82cf9c171ef545522960e63287b79a89456b51
brian.shourd <brian.shourd@b2d95fcb-6840-0410-91c4-7d0ded462287>
2008-02-28 05:33:11 +0000

    Added a GPL3 license to the source code (everywhere) and put it at the tope of every file along with a Copyright label. This should be the last major commit for the night (I hope).

--- trunk/AutoRPG/Map.h
+++ trunk/AutoRPG/Map.h
@@ -1,6 +1,26 @@
 #ifndef MAP_H
 #define MAP_H
 
+/*
+Copyright 2007, 2008 Andrew Allen and Brian Shourd
+
+This file is part of AutoRPG.
+
+AutoRPG is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+AutoRPG is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with AutoRPG (Called LICENSE.txt).  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+
 #include <vector>
 #include <string>
 #include "Sprite_Sheet.h"
@@ -11,13 +31,9 @@ class Map
 private:
     std::vector< std::vector<int> > spriteIds;
     Sprite_Sheet* spriteSheet;
-    int StringToInt(std::string in);
     Point spriteDim;
 public:
-    Map(std::string spriteIdsFile);
     Map(std::string mapFile, std::string pictureFile);
-    void AssignSpriteSheet(Sprite_Sheet* sheet);
-    void AssignSpriteSheet(int wOfSprite, int hOfSprite, std::string file, int red = 0, int green = 255, int blue = 255);
     void ApplyMap(int x, int y, int h, int w, SDL_Surface* destination);
     Point GetDim();
 };
--- trunk/AutoRPG/Sprite_Sheet.cpp
+++ trunk/AutoRPG/Sprite_Sheet.cpp
@@ -1,3 +1,23 @@
+/*
+Copyright 2007, 2008 Andrew Allen and Brian Shourd
+
+This file is part of AutoRPG.
+
+AutoRPG is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+AutoRPG is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with AutoRPG (Called LICENSE.txt).  If not, see
+<http://www.gnu.org/licenses/>.
+*/
+
 #include "Sprite_Sheet.h"
 #include "constants.h"
 #include "Point.h"
@@ -21,6 +41,11 @@ Sprite_Sheet::Sprite_Sheet(int wOfSprite, int hOfSprite, std::string file, int r
 		sheet = SDL_DisplayFormat(loadedImage);
 		SDL_FreeSurface(loadedImage);   //Free this memory
 	}
+
+    if (sheet == NULL)
+    {
+        printf("Sheet is not initialized for some reason\n");
+    }
 
     int numx = (sheet->w)/wOfSprite;  //Number of sprites in the x direction
     int numy = (sheet->h)/hOfSprite;  //Number of sprites in the y direction
